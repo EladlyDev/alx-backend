df2186717fe3859effe1ceb76d0b65e2dbefc2b8
Mohamed Eladly <msrtv365@yahoo.com>
2024-06-24 12:14:22 +0300

    Done with task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Simple helper function """
+import typing
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple representing the start and end indices
+    for a given page and page size.
+    """
+    end = page * page_size
+    start = end - page_size
+    return tuple([start, end])
