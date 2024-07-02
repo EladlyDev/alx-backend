0ca6d712c419c39f31818a51e2cd4d7a59b083a4
Mohamed Eladly <msrtv365@yahoo.com>
2024-07-02 15:58:08 +0300

    Done with task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" 2-app simple flask app with babel """
+""" 3-app simple flask app with babel """
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 
 class Config(object):
-    """ Config class """
+    """ Config class for languages"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -19,14 +19,16 @@ babel = Babel(app)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """ 2-app simple flask app """
-    return render_template('2-index.html')
+    """ 3-app simple flask app """
+    t = _('Welcome to Holberton')
+    h = _('Hello world!')
+    return render_template('3-index.html', home_header=h, home_title=t)
 
 
 @babel.localeselector
 def get_locale():
-    """ get_locale """
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    """ get_locale gets the best match language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
