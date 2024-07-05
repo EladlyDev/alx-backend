8b33f81949322f93a818c4222ba9c23062bf62bc
Mohamed <eladlydev@gmail.com>
2024-07-05 21:19:07 +0300

    Trying to fix the checker

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,33 +1,48 @@
 #!/usr/bin/env python3
-""" 2-app simple flask app with babel """
-
-from flask import Flask, render_template, request
+""" Basic Babel setup """
 from flask_babel import Babel
+from flask import Flask, render_template
+from typing import List, Any
 
 
-class Config(object):
-    """ Config class """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    """
+    Configuration class for the application.
+
+    Attributes:
+        LANGUAGES (list): List of supported languages for the application.
+        BABEL_DEFAULT_LOCALE (str): Default locale for the application.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone for the application.
+    """
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 babel = Babel(app)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """ 2-app simple flask app """
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Selects a language translation to use for the request.
+
+    Returns:
+        str: The best language translation to use for the request.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> Any:
+    """
+    Renders the '0-index.html' template.
+    """
     return render_template('2-index.html')
 
 
-@babel.localeselector
-def get_locale():
-    """ get_locale """
-    return request.accept_languages.best_match(Config.LANGUAGES)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
