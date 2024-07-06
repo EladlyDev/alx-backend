d68c3cf52035236fd95eb1fde223f8276b7111e9
Mohamed <eladlydev@gmail.com>
2024-07-06 11:17:32 +0300

    Fixing task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,35 +1,51 @@
 #!/usr/bin/env python3
-""" 3-app simple flask app with babel """
-
+""" Basic Babel setup """
+from flask_babel import Babel, gettext
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from typing import List, Any
 
 
-class Config(object):
-    """ Config class for languages"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    """
+    Configuration class for the application.
+
+    Attributes:
+        LANGUAGES (list): List of supported languages for the application.
+        BABEL_DEFAULT_LOCALE (str): Default locale for the application.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone for the application.
+    """
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
+babel = Babel()
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """ 3-app simple flask app """
-    t = _('Welcome to Holberton')
-    h = _('Hello world!')
-    return render_template('3-index.html', home_header=h, home_title=t)
+def get_locale() -> str:
+    """
+    Selects a language translation to use for the request.
 
-
-@babel.localeselector
-def get_locale():
-    """ get_locale gets the best match language"""
+    Returns:
+        str: The best language translation to use for the request.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run()
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index() -> Any:
+    """
+    Renders the '3-index.html' template.
+    """
+    t = gettext('Welcome to Holberton')
+    h = gettext('Hello world!')
+    return render_template('3-index.html', home_title=t, home_header=h)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
