e60d83952786d0e5443d3958a14f8952b9047b2a
Mohamed <eladlydev@gmail.com>
2024-07-25 13:16:23 +0300

    Done with task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+"""1. FIFO caching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    A basic cache implementation that extends the BaseCashing class.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache with the specified key.
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.pop(key)
+            self.cache_data[key] = item
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded = self.cache_data.popitem()[0]
+            print(f"DISCARD: {discarded}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves the item from the cache with the specified key.
+        """
+        return self.cache_data.get(key, None)
