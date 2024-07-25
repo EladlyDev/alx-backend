c694f2b98ef7da35608ad482bbe3b97bf7c209bd
Mohamed <eladlydev@gmail.com>
2024-07-25 10:58:24 +0300

    Done with task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" contains the BasicCashe class """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A basic cache implementation that extends the BaseCashing class.
+    """
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache with the specified key.
+        """
+        if (key and item):
+            self.cache_data.update({key: item})
